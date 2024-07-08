597f0ebf56049bf66293d49caa66b7c1113a2a05
joaocapitaneo <joaocapitaneo@gmail.com>
2024-07-08 17:25:53 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+from controler.user_c import UsuarioController
+from model.usuario_m import UsuarioModel
+from view.user_v import UsuarioView
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Gerenciamento de Usuários")
+    root.geometry("400x300")
+
+    model = UsuarioModel()
+    view = UsuarioView(root)
+    controller = UsuarioController(view, model)
+
+    root.mainloop()
+    model.fechar_conexao()
